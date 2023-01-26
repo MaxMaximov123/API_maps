21dba354f707f70357cf0318288ab3916cf21c4d
student <maxss.kzn@gmail.com>
2023-01-26 21:49:01 +0300

    add #8

--- #8/main.py
+++ #8/main.py
@@ -23,8 +23,8 @@ class Example(QMainWindow):
 		self.path = 'img.png'
 		self.slices = ['map', 'sat', 'skl', 'trf']
 		self.slice_ind = 0
-		self.adress = 'Бутлерова, Казань'
-		self.current_pos, D_X, D_Y = get_obj(self.adress)
+		self.address = 'Бутлерова, Казань'
+		self.current_pos, D_X, D_Y, address_obg = get_obj(self.address)
 		self.current_pos = list(map(float, self.current_pos.split(' ')))
 		self.delta = 1
 		self.step = 1.3
@@ -32,13 +32,15 @@ class Example(QMainWindow):
 		self.save_map()
 
 	def save_map(self, point=False):
-		adress = self.lineEdit.text()
-		if point and adress:
-			self.current_pos, D_X, D_Y = get_obj(adress)
+		address = self.lineEdit.text()
+		if point and address:
+			self.current_pos, D_X, D_Y, address_obg = get_obj(address)
 			self.current_pos = list(map(float, self.current_pos.split(' ')))
 			self.delta = min(D_X, D_Y)
 			self.point = f'{self.current_pos[0]},{self.current_pos[1]}'
-		obj_coodrinates, d_x, d_y = get_obj(f'{self.current_pos[0]},{self.current_pos[1]}')
+			self.textEdit.clear()
+			self.textEdit.insertPlainText(address_obg)
+		obj_coodrinates, d_x, d_y, address_obg = get_obj(f'{self.current_pos[0]},{self.current_pos[1]}')
 		x, y = obj_coodrinates.split(" ")
 		map_params = {
 			"ll": ",".join([x, y]),
@@ -55,9 +57,8 @@ class Example(QMainWindow):
 		self.image.setPixmap(self.pixmap)
 
 	def initUI(self):
-		self.setGeometry(200, 200, *SCREEN_SIZE)
 		self.setWindowTitle('Карты')
-		self.image.resize(600, 470)
+		self.image.resize(600, 450)
 		self.pixmap = QPixmap(self.path)
 		self.image.setPixmap(self.pixmap)
 		self.pushButton.clicked.connect(partial(self.save_map, point=True))
@@ -103,6 +104,7 @@ class Example(QMainWindow):
 		self.point = None
 		self.save_map()
 		self.lineEdit.setText('')
+		self.textEdit.clear()
 
 
 def except_hook(cls, exception, traceback):
