c61a81f087d9a9c88dc1aaf399751fd687a10ace
Max Maximov <maxss.k2n@yandex.ru>
2023-01-26 14:37:19 +0300

    add #2

---
+++ #2/get_obj.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def get_obj(req):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": req,
+        "format": "json"}
+
+    r = requests.get(geocoder_api_server, params=geocoder_params)
+    json_response = r.json()
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    delta_x = abs(
+        float(toponym['boundedBy']['Envelope']['lowerCorner'].split()[0]) -
+        float(toponym['boundedBy']['Envelope']['upperCorner'].split()[0]))
+    delta_y = abs(
+        float(toponym['boundedBy']['Envelope']['lowerCorner'].split()[1]) -
+        float(toponym['boundedBy']['Envelope']['upperCorner'].split()[1]))
+
+    coords = toponym["Point"]["pos"]
+
+    return coords, delta_x, delta_y
+
---
+++ #2/main.py
@@ -0,0 +1,55 @@
+import sys
+import json
+import pygame as pg
+import os
+from get_obj import get_obj
+import requests
+from PIL import Image
+
+PATH = 'data/img.png'
+delta = 10
+STEP = 1.3
+ADRESS = 'Бутлерова, Казань'
+
+
+def save_map(adress, path, delta0=delta):
+    obj_coodrinates, d_x, d_y = get_obj(adress)
+    x, y = obj_coodrinates.split(" ")
+    map_params = {
+        "ll": ",".join([x, y]),
+        "spn": ",".join([str(delta), str(delta * 3 / 4)]),
+        "l": "map",
+        "pt": f"{','.join(obj_coodrinates.split())},pm2dgl"
+    }
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+    with open(path, 'wb') as f:
+        f.write(response.content)
+
+
+if __name__ == '__main__':
+    pg.init()
+    save_map(ADRESS, PATH)
+    screen = pg.display.set_mode((600, 450))
+    screen.blit(pg.image.load(PATH), (0, 0))
+    pg.display.flip()
+    f = True
+    while f:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                f = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    if delta * STEP <= 45:
+                        delta *= STEP
+                        save_map(ADRESS, PATH, delta0=delta)
+                        screen.blit(pg.image.load(PATH), (0, 0))
+                        pg.display.flip()
+                if event.key == pg.K_DOWN:
+                    if delta / STEP > 0:
+                        delta /= STEP
+                        save_map(ADRESS, PATH, delta0=delta)
+                        screen.blit(pg.image.load(PATH), (0, 0))
+                        pg.display.flip()
+    pg.quit()
+
